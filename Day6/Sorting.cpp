6d7b6a6643b5b1780ec60dc386f7d8e10d335c06
Yash Malviya <malviyarajyash27@gmail.com>
2025-06-16 11:20:48 +0530

    in this commit i am pusing an insertion sort

--- Day6/Sorting.cpp
+++ Day6/Sorting.cpp
@@ -23,13 +23,24 @@ void bubbleSort(int arr[], int n){
         if(!swapped) break;
     } 
 }
+void insertionSort(int arr[], int n){
+    for(int i = 1; i<=n-1; i++){
+        int key = arr[i];
+        int j = i-1;
+        while(arr[j]>key && j>=0){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
 int main(){
     int n;
     cout<<"Enter the nnumber.......!!"<<endl;
     cin>>n;
     int arr[n];
     for(int i = 0; i<n;i++)cin>>arr[i];
-    bubbleSort(arr,n);
+    insertionSort(arr,n);
     for(int i = 0; i<n;i++)cout<<arr[i]<<" ";
     return 0 ;
 }
